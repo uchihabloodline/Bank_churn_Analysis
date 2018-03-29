a50fbc274235d11ce8ffc4eea389ff5ea4de6116
shinobi shivam <geetapandey595@gmail.com>
2018-03-29 19:07:38 +0530

    Update ann.py

--- ann.py
+++ ann.py
@@ -30,7 +30,6 @@ X = onehotencoder.fit_transform(X).toarray()
 #Remove dummie variable
 X = X[:, 1:]
 
-# Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
@@ -40,7 +39,7 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-#  ------------------Making the ANN -------------------------
+#Ann ...
 # Importing Keras
 import keras
 from keras.models import Sequential
@@ -85,8 +84,5 @@ cm = confusion_matrix(y_test, y_pred)
 
 # for plotting of graphs import matplotlib as plt and pot the
 #  used feature matrix
-
-
-##  1545 + 136 correct predictions and 230 + 50 incorrect
 
 # Comput accuracy by 1545 + 136 / 2000 predictions == 0.8405 % accuracy.
